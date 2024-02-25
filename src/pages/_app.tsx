03e7f6410c448dc3b367fa73f7807e7b1c1b0aed
i-ice-bear <andydowsen47us@gmail.com>
2024-02-25 22:54:24 +0530

    Initialised navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import { NextUIProvider } from "@nextui-org/react";
+import NavbarNavigation from "../../Components/Routed/Navbar.navigation";
 
+const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <NextUIProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+         <NavbarNavigation />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </NextUIProvider>
+    </>
+  );
 }
