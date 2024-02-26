3678f221440e837dd849a10cb096ac2ffe81007b
i-ice-bear <andydowsen47us@gmail.com>
2024-02-26 11:45:21 +0530

    Initialised chakratheme[configured]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,47 +1,31 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
 import { NextUIProvider } from "@nextui-org/react";
 import NavbarNavigation from "../../Components/Routed/Navbar.navigation";
-import DevelopmentAlertBanner from '../../Components/Shared/Component/Banner.dev.tsx';
+import defaultCustomExtendTheme from "../../Components/Theme/Custom.chakra.tsx";
+import DevelopmentAlertBanner from "../../Components/Shared/Component/Banner.dev.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   const navbarLinkObject = [
-    {
-      link: "/",
-      name: "Features",
-    },
-    {
-      link: "/",
-      name: "Customers",
-    },
-    {
-      link: "/",
-      name: "Integrations",
-    },
-    {
-      link: "/",
-      name: "Services ⭐",
-    },
+    { link: "/", name: "Features" },
+    { link: "/", name: "Customers" },
+    { link: "/", name: "Integrations" },
+    { link: "/", name: "Services ⭐" },
   ];
   return (
     <>
-      <style jsx global>{`
-        html {
-          font-family: ${inter.style.fontFamily};
-        }
-      `}</style>
-      <div className="development-informative-section">
-        {/* to be deleted when the application developmentw will be completed! */}
-        <DevelopmentAlertBanner />
-      </div>
       <NextUIProvider>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <NavbarNavigation navigationLinks={navbarLinkObject} />
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <div className="development-informative-section">
+          <DevelopmentAlertBanner />
+        </div>
+        <ChakraProvider theme={defaultCustomExtendTheme}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <NavbarNavigation navigationLinks={navbarLinkObject} />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </ChakraProvider>
       </NextUIProvider>
     </>
   );
