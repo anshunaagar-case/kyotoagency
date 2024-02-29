be7b44b6d4d72c0e4f63b31573715133f606c5fa
i-ice-bear <andydowsen47us@gmail.com>
2024-02-29 17:00:21 +0530

    Added[maskcursor] effect with animations and framertionintegration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,33 @@ import { NextUIProvider } from "@nextui-org/react";
 import NavbarNavigation from "../../Components/Routed/Navbar.navigation";
 import DevelopmentAlertBanner from "../../Components/Shared/Component/Banner.dev.tsx";
 import { Inter } from "next/font/google";
+import useMousePointerCoordinates from "@/utils/UseMousePosition.mouseDom.tsx";
+import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const CursorableMouse = () => {
+  const { xCoordinate, yCoordinate } = useMousePointerCoordinates();
+  const maskSize = 50;
+  return (
+    <>
+      <motion.div
+        animate={{
+          WebkitMaskPosition: `${xCoordinate - maskSize / 4}px ${
+            yCoordinate - maskSize / 4
+          }px`,
+          transition: {
+            type: "tween",
+            duration: 0.25,
+          },
+        }}
+        className="maskClipper"
+      >
+        jack
+      </motion.div>
+    </>
+  );
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   const navbarLinkObject = [
@@ -27,6 +52,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <DevelopmentAlertBanner />
         </div>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <CursorableMouse />
           <NavbarNavigation navigationLinks={navbarLinkObject} />
           <Component {...pageProps} />
         </ThemeProvider>
---
+++ src/utils/UseMousePosition.mouseDom.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+const useMousePointerCoordinates = () => {
+  const [mouseValue, setMouseValue] = useState({xCoordinate: 0, yCoordinate: 0});
+  const updateMouseEvent = (event: any) => {
+    setMouseValue({ xCoordinate: event.clientX, yCoordinate: event.clientY });
+  };
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMouseEvent);
+  });
+  return mouseValue;
+};
+
+export default useMousePointerCoordinates;
